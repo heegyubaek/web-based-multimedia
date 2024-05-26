ab62a78b37a1c861ebbcc7c7e4980da07ebb7eb9
heegyubaek <bhg.king47@gmail.com>
2024-05-26 19:44:18 +0900

    add github-page configure

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://heegyubaek.github.io/web-based-multimedia'
+      : '',
+  output: 'export',
+};
 
 module.exports = nextConfig;
